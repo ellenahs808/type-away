17df742802360ef63df9c772a05a4056731eccb5
Shanelle Valencia <sgev808@hawaii.edu>
2020-09-11 14:22:00 -0700

    modify game

--- src/helper.js
+++ src/helper.js
@@ -1,40 +1,19 @@
-// const helper = {
-//     ww: window.innerWidth,
-//     wh: window.innerHeight,
-// };
-
-// helper.random = (min, max) => {
-//     return Math.floor(Math.random() * (max - min)) + min;
-// };
-
-// helper.wrapper = document.getElementById("ws-wrapper");
-
-// // diagonal distance of view for spawn area
-// helper.radius = Math.sqrt(helper.ww * helper.ww + helper.wh * helper.wh) / 4;
-
-
-
-// export default helper;
-
-
-class Helper {
-  constructor(ww, wh) {
-    this.ww = window.innerWidth;
-    this.wh = window.innerHeight;
-  }
-
-  random(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-  };
-
-  wrapper() {
-      document.getElementById("ws-wrapper");
-  }
-
-  radius() {
-      Math.sqrt(helper.ww * helper.ww + helper.wh * helper.wh) / 4;
-  } 
+const helper = {
+    x: 1800,
+    y: 900
 }
 
 
+helper.random = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+
+helper.wrapper = document.getElementById('ws-wrapper');
+
+
+helper.radius = Math.sqrt(helper.x * helper.x + helper.y * helper.y) / 4;
+
+
 export default helper;
+
--- src/index.js
+++ src/index.js
@@ -1,31 +1,24 @@
-
-import Game from './game';
-
-
-document.addEventListener("DOMContentLoaded", () => {
-    const canvas = document.getElementById("game-canvas");
-    let ctx = canvas.getContext("2d");
-    let game = new Game(canvas, ctx);
-    game.drawSquare(ctx);
-    game.createWords();
-    game.testThis();
-    // game.render();
-    // game.loop(frames)
-    game.createWords()
-    game.play()
+// import Game from './not_game';
 
 
 
-    // for testing
-    window.canvas = canvas;
-    window.ctx = ctx;
-    window.Game = Game;
-    //
-    
-
-});
+// // document.addEventListener("DOMContentLoaded", () => {
+//     const canvas = document.getElementById("game-canvas");
+//     let ctx = canvas.getContext("2d");
+//     let game = new Game(canvas, ctx);
 
 
-// const canvas = document.getElementById("game-canvas");
+//     // game.drawSquare(ctx);
+//     game.testThis();
+//     // game.gameLoop();
+//     // game.play();
+//     // timerId = setInterval(game.drawWord(), 3000)
+//     // timerId = setInterval(game.gameLoop(), 3000)
+//     // timerId = setInterval(game.gameLoop(), 3000)
+//     game.play()
 
-// new Game(canvas);
+// // });
+
+
+
+import helper from './helper';
